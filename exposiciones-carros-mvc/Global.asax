1695038b5186aecafba0185c2d923504beac7835
Carlos Quintero <carlosquintero95nic@gmail.com>
2021-07-07 09:35:20 -0600

    Add project files.

---
+++ exposiciones-carros-mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="exposiciones_carros_mvc.MvcApplication" Language="C#" %>
---
+++ exposiciones-carros-mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(exposiciones_carros_mvc.Startup))]
+namespace exposiciones_carros_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
